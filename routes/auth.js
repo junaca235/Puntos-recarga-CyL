48d4d7a8df3593b485adee4757a2197708502ebb
junaca235 <juanccaceres@grupotecopy.es>
2023-04-24 10:40:15 +0200

    Aqui se encuentra el build de frontend implementado al backend para sacarlo a producción

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const { Router } = require("express");
+const { crearUsuario, loginUsuario, revalidarToken, buscarPuntos, guardarPunto, eliminarPunto } = require("../controllers/auth.controller");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+
+const router = Router();
+
+//Crear usuario
+router.post( "/new", [
+    check("name", "El nombre es obligatorio").notEmpty(),
+    check("password", "La contraseña es obligatorio").isLength({ min: 6 }),
+    validarCampos
+], crearUsuario )
+
+//Login de ususario
+router.post( "/",  [
+    check("name", "El nombre es obligatorio"),
+    check("password", "La contraseña es obligatorio").isLength({ min: 6 }),
+], loginUsuario )
+
+//Validar y revalidar token
+router.get( "/renew", validarJWT, revalidarToken )
+
+//Buscar puntos
+router.post( "/searchPuntos",  [
+    check("user", "Nombre de usuario obligatorio")
+], buscarPuntos );
+
+//Crear punto
+router.post( "/newPunto",  [
+    check("user", "Nombre de usuario obligatorio"),
+    check("recordid", "Id del punto obligatorio")
+], guardarPunto );
+
+//Borrar punto
+router.post( "/deletePunto",  [
+    check("user", "Nombre de usuario obligatorio"),
+    check("recordid", "Id del punto obligatorio")
+], eliminarPunto );
+
+
+module.exports = router
