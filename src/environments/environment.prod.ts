1fc352fa32d3c6c6b567c3dc7b3accc5b1eb27f2
junaca235 <juanccaceres@grupotecopy.es>
2023-04-24 10:12:38 +0200

    Aqui se encuentra todo el código de fontend antes de ser implementado en el backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     mapboxToken: "pk.eyJ1IjoianVuY2EyMzUiLCJhIjoiY2xmdjBpa2VnMDJrYzNrbzMwMmEyOXlmZiJ9.GJ8cigJUn2cUWxd_Wx_5VQ",
-    mongoUrl: "http://localhost:4000/api",
+    mongoUrl: "http://puntos-recarga-cyl-production.up.railway.app/api",
     jcylUrl: "https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=puntos-de-recarga-del-vehiculo-electrico&rows=123"
 };
